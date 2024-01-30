67f3536615a60480875f27c95ba63bd24221ac49
leesehwi <leesehwi@minigate.net>
2024-01-30 12:30:20 +0900

    멋진수정

--- src/main/java/com/example/blog2/Blog2Application.java
+++ src/main/java/com/example/blog2/Blog2Application.java
@@ -3,9 +3,18 @@ package com.example.blog2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-public class Blog2Application {
+public class Blog2Application extends SpringBootServletInitializer implements WebApplicationInitializer {
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Blog2Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Blog2Application.class, args);
